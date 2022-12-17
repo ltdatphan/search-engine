5e7f766cf7f0c5d00c8030c51b27be9fd08525f6
David Phan <29266892+ltdatphan@users.noreply.github.com>
2022-12-17 15:28:44 -0500

    updated api

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def home():
 
 
 @app.route("/search", methods=["GET"])
-@cross_origin(origins=["https://inquisitive-bunny-f80acf.netlify.app"])
+@cross_origin(origins=["https://inquisitive-bunny-f80acf.netlify.app", "http://localhost:3000"])
 def search():
     query = request.args["query"]
 
@@ -48,9 +48,31 @@ def search():
     return jsonify({"query": query, "result": results})
 
 
-# if __name__ == "__main__":
-#     # filehandler = open(Path("./trained_models/bm25_model.obj"), "rb")
-#     # bm25_model = pickle.load(filehandler)
-#     # filehandler = open(Path("./trained_models/nb_model.obj"), "rb")
-#     # nb_model = pickle.load(filehandler)
-#     app.run()
+@app.route("/search_scores", methods=["GET"])
+@cross_origin(origins=["https://inquisitive-bunny-f80acf.netlify.app", "http://localhost:3000"])
+def search_scores():
+    query = request.args["query"]
+
+    # Preprocessing
+    userInput = " ".join(remove_stopword(tokenize(query)))
+
+    # Get the probability for each category
+    res = nb_model.predict_proba([userInput])
+
+    # Get the top 5 most relevant categories and their scores
+    relevant_topics_list = sorted(zip(nb_model.classes_, res[0]), key=lambda x: x[1], reverse=True)[:5]
+    print(relevant_topics_list)
+    # Only extract the text of the 5 most relevant categories
+    relevant_topics = []
+    for topic in relevant_topics_list:
+        relevant_topics.append(topic[0])
+
+    tokenized_query = userInput.split(" ")
+
+    # Get the 10 most relevant documents
+    results = bm25_model.get_highest_relevant_k_scores(tokenized_query, relevant_topics, 10)
+
+    # Get 10 best topics from Naive Bayes
+    top_10_nb_scores = sorted(zip(nb_model.classes_, res[0]), key=lambda x: x[1], reverse=True)[:10]
+
+    return jsonify({"query": query, "result": results, "nb_result": top_10_nb_scores})
