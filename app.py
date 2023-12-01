d49b148780ee8c68719209b88c2f5b0ce484ac61
David Phan <29266892+ltdatphan@users.noreply.github.com>
2023-12-01 16:11:00 -0500

    rollback version

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def home():
 
 
 @app.route("/search", methods=["GET"])
-@cross_origin(origins=["https://search-engine-cps842.netlify.app/", "http://localhost:3000"])
+@cross_origin(origins=["https://search-engine-cps842.netlify.app/"])
 def search():
     query = request.args["query"]
 
